04ab72212a596887bddb067db60e8d1e530b3c0f
FlokiTV <dawysonw@gmail.com>
2025-02-22 14:47:07 -0300

    feat: new table

--- src/routes/app/clientes/+page.svelte
+++ src/routes/app/clientes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
+	import IconCheckAll from '@/icons/IconCheckAll.svelte';
+	import IconMagnify from '@/icons/IconMagnify.svelte';
 	import IconPlus from '@/icons/IconPlus.svelte';
+	import IconSeemore from '@/icons/IconSeemore.svelte';
 	import IconTrash from '@/icons/iconTrash.svelte';
 </script>
 
@@ -25,9 +28,14 @@
 		</button>
 	</div>
 	<div class="flex justify-between pb-2 md:mt-6">
-		<div class="min-w-xs max-md:w-full">
+		<form class="relative flex min-w-xs max-md:w-full">
 			<input placeholder="Pesquise por nome ou email" type="text" class="input w-full" />
-		</div>
+			<button
+				class="absolute top-0 right-1 bottom-0 flex w-8 cursor-pointer items-center opacity-30"
+			>
+				<IconMagnify />
+			</button>
+		</form>
 		<button
 			class="btn btn-error right-[80px] bottom-6 z-10 flex max-md:fixed max-md:h-auto max-md:rounded-full max-md:p-2"
 		>
@@ -37,9 +45,38 @@
 			</div>
 		</button>
 	</div>
-	<div class="mt-2 max-h-[80vh] overflow-x-auto">
-		<div class="bg-base-100 rounded-lg p-4 md:p-2">
+	<div class="mt-2">
+		<button
+			class="bg-base-100 border-base-300 fixed bottom-6 left-4 z-10 size-10 cursor-pointer rounded-full border p-1 md:hidden"
+		>
+			<IconCheckAll />
+		</button>
+		<div class="bg-base-100 flex items-center gap-2 p-4 md:p-2">
 			<input class="checkbox" type="checkbox" />
+			<div class="hidden md:block">Nome</div>
+			<div class="grow">Email</div>
+			<div>Ações</div>
+		</div>
+		{#each Array.from({ length: 10 }) as o}
+			<div class="bg-base-100 border-base-300 border-t p-4 md:p-2 flex gap-2">
+				<input class="checkbox" type="checkbox" />
+				<div class="whitespace-nowrap hidden md:block">nome sobrenome grande</div>
+				<div class="grow overflow-hidden">
+					<div class="truncate">emailmtgrandeasdasdasddasd@example.com</div>
+				</div>
+				<div>
+					<div class="size-6">
+						<IconSeemore />
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
+	<div class="flex justify-between pb-20 max-md:flex-col mt-2">
+		<div class="text-center">1 de 10 linhas selecionadas</div>
+		<div class="text-center">
+			<button class="btn">Anterior</button>
+			<button class="btn btn-neutral">Próxima</button>
 		</div>
 	</div>
 </div>
