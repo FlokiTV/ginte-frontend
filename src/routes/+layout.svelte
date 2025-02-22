0c53df56294822f4c512f84c624de8bf6bfb2386
FlokiTV <dawysonw@gmail.com>
2025-02-22 08:19:36 -0300

    feat: add theme controller

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import '@/app.css';
+	import ThemeController from '@/components/themeController.svelte';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="h-screen w-screen bg-gray-200">
+	<div class="fixed right-2 top-2">
+		<ThemeController />
+	</div>
+	{@render children()}
+</div>
