cfdf2c4418509ee3104b49a25bd0e6f4db0029fd
FlokiTV <dawysonw@gmail.com>
2025-02-24 09:50:27 -0300

    feat: new api lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,29 @@
-// place files you want to import through the `$lib` alias in this folder.
+export interface Client {
+	name: string;
+	email: string;
+	phone: string;
+	address: string;
+}
+
+export const api = {
+	url: 'http://localhost:3000',
+	token: '',
+	fetch(url: string, method: string = 'GET', body?: any) {
+		const headers: Record<string, string> = {
+			'Content-Type': 'application/json'
+		};
+		if (this.token) {
+			headers['Authorization'] = `Bearer ${this.token}`;
+		}
+		return fetch(this.url + url, { method, body, headers });
+	},
+	clients: {
+		create(dto: Client) {},
+		read(id: string) {},
+		list() {
+			return api.fetch('/clients');
+		},
+		update(dto: Client) {},
+		delete(id: string) {}
+	}
+};
